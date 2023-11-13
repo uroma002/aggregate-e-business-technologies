9073c3b1b556e669533ec51a0ed87af8f1366f1c
Uroma002 <146116449+uroma002@users.noreply.github.com>
2023-11-13 17:14:07 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,26 @@
+    return data
+
+import random
+def generate_random_data():
+
+
+
+def main():
+    main()
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
+}
+        int[] data = new int[10];
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+
+    public static void main(String[] args) {
+    }
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+        }
